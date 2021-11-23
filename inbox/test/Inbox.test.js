3e8d1302adcef0a2e532380973e05a1d15be2d02
J <jradingrant@gmail.com>
2021-11-23 17:24:55 +0000

    initialising inbox.test.js - Finishing compile.js

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,1 +1,8 @@
 // contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+//capital W due to it being a contructor function, web3 uses this contructor
+//function to make instances of the web3 library
+const Web3 = require('web3');
+//creates an instance of web 3 and tells it to attempt to connect to the local test network.
+const web3 = new Web3(ganache.provider());  
