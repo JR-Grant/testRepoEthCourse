e31101152b7bd4922298f354293921b4140e13b2
J <jradingrant@gmail.com>
2021-11-23 17:55:47 +0000

    Testing mocha functions

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -6,3 +6,35 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 //creates an instance of web 3 and tells it to attempt to connect to the local test network.
 const web3 = new Web3(ganache.provider());
+
+//creating an arbitrary class for testing purposes
+class Car {
+  park() {
+    return 'stopped';
+  }
+
+  drive(){
+    return 'vroom';
+  }
+}
+
+//ensures that variable is available over the whole scope.
+//let as opposed to const as the value changes throughout.
+let car;
+//testing class Car
+//running npm run test wull run the tests
+beforeEach(() => {
+  //initialises object before each it statement
+  car = new Car();
+});
+
+describe('Car', () => {
+  it('can park', () => {
+    //passess if both values are equal
+    assert.equal(car.park(), 'stopped');
+  });
+
+  it('can drive', () =>{
+    assert.equal(car.drive(), 'vroom');
+  });
+});
